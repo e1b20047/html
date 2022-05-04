573dac27887919819afc1fe4a25e9010317cd141
e1b20047 <95567767+e1b20047@users.noreply.github.com>
2022-05-04 20:20:01 +0900

    Add files via upload

---
+++ dbs_c/enter.js
@@ -0,0 +1,25 @@
+let element=document.getElementById("entra");
+console.log(element.value);
+
+let output=document.getElementById("Bye");
+let checkButton = document.getElementById('clickButton');
+checkButton.addEventListener('click',change);
+
+const pattern=/e1b[0-9]{5}[=|-|(|!|-|*]*>/;
+const noise=/\\r/;
+
+
+function change(){
+    output.value="";
+    let text=document.getElementById("entra").value.replace(/\r\n|\r/g, "\n");
+    let lines=text.split('\n');    
+    for(let i=0;i<lines.length;i++){
+        let chst=lines[i];
+        
+        if(pattern.test(chst)){
+            if(noise.test(chst)) continue;
+            output.value+=chst+"\n";
+        }
+    }
+    
+}
